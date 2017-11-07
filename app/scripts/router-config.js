3a85de5e447105d06ee6525446b327c4e8128421
Godlyn Paul <godlynpaul@gmail.com>
2017-11-07 19:21:22 +0530

    file base for routing

--- app/scripts/router-config.js
+++ app/scripts/router-config.js
@@ -2,5 +2,25 @@ define([
 	'./pkRootModule'
 ],function(popKartRootModule){
 	'use strict'
+	popKartRootModule.config(['$routeProvider','$locationProvider',
+		function($routeProvider,$locationProvider){
+			$routeProvider
+				.when('/mobiles',{
+					controller:'',
+					templateUrl:'./app/views/mobiles.html'
+				})
+				.when('/view2',{
+					controller:'',
+					templateUrl:'./app/views/view2.html'
+				})
+				.when('/view3',{
+					controller:'',
+					templateUrl:'./app/views/view3.html'
+				})
+ 				.otherwise({redirectTo:'/mobiles'})
+  		// $locationProvider.html5Mode(false);
+	  	$locationProvider.hashPrefix('');
+			$locationProvider.html5Mode({enabled: true, requireBase: true, rewriteLinks: true});
+		}]);
 	return popKartRootModule;
 })
